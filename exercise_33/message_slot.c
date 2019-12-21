1c267075ab35b01f5a6858a187549960fea500ee
Nevo <nevoraz@gmail.com>
2019-12-21 10:26:09 +0200

    22

--- exercise_33/message_slot.c
+++ exercise_33/message_slot.c
@@ -6,10 +6,10 @@
 #define __KERNEL__
 #undef MODULE
 #define MODULE
-//#include <stdio.h>
+#include <linux/stdio.h>
 #include <string.h>
-//#include <stdlib.h>
-//
+#include <linux/stdlib.h>
+
 #include <linux/kernel.h>   /* We're doing kernel work */
 #include <linux/module.h>   /* Specifically, a module */
 #include <linux/fs.h>       /* for register_chrdev */
