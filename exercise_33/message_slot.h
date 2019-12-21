c7835f9d8df76b2d7d8e8fd1d9346dc368903f72
Nevo <nevoraz@gmail.com>
2019-12-21 11:13:24 +0200

    106 + header file

--- exercise_33/message_slot.h
+++ exercise_33/message_slot.h
@@ -1,20 +1,16 @@
 #ifndef message_slot_h
 #define message_slot_h
-
 #include <linux/ioctl.h>
-
 // The major device number..
 // We don't rely on dynamic registration
 // any more. We want ioctls to know this
 // number at compile time.
 #define MAJOR_NUM 240
-
+#define MINOR_NUM
 // Set the message of the device driver
-#define IOCTL_SET_ENC _IOW(MAJOR_NUM, 0, unsigned long)
-
-#define DEVICE_RANGE_NAME "char_dev" //TODO: check this line
+#define IOCTL_MSG_SLOT_CHANNEL(MAJOR_NUM, MINOR_NUM, int)
+#define DEVICE_RANGE_NAME "char_dev" // TODO: check this line
 #define BUF_LEN 128
-#define DEVICE_FILE_NAME "simple_char_dev" //TODO: check this line
-#define SUCCESS 0 //TODO: check this line
-
+#define DEVICE_FILE_NAME "slot" // TODO: check this line
+#define SUCCESS 0 // TODO: check this line
 #endif /* message_slot_h */
