b7f1c645118c3c60748b74b1d95f9908b5fe0585
Nevo <nevoraz@gmail.com>
2019-12-26 20:35:10 +0200

    added reader.c

--- exercise_33/message_reader.c
+++ exercise_33/message_reader.c
@@ -1,1 +1,33 @@
+#include "message_slot.h"
+
+#include <fcntl.h>      /* open */
+#include <unistd.h>     /* exit */
+#include <sys/ioctl.h>  /* ioctl */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **arg){
+    int file_desc;
+    int ret_val;
+    char messsage[100];
+    
+    file_desc = open( "/dev/"DEVICE_FILE_NAME, O_RDWR );
+    if( file_desc < 0 )
+    {
+        printf ("Can't open device file: %s\n",
+                DEVICE_FILE_NAME);
+        exit(-1);
+    }
+    ret_val = ioctl( file_desc, IOCTL_MSG_SLOT_CHANNEL, 12);
+    ret_val = read(file_desc, messsage, 5);
+    //  ret_val = read(  file_desc, NULL, 100 );
+    //  ret_val = ioctl( file_desc, IOCTL_SET_ENC, 1);
+    //  ret_val = write( file_desc, "Hello", 5);
+    //  ret_val = read(  file_desc, NULL, 100 );
+    //
+    printf("read succesfully the message: %s\n", messsage);
+    close(file_desc);
+    return 0;
+}
+
 
