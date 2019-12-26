e78aa27a09001166404ab8d762880fedc7a5897c
Nevo <nevoraz@gmail.com>
2019-12-26 21:57:52 +0200

    f

--- exercise_33/message_slot.c
+++ exercise_33/message_slot.c
@@ -102,7 +102,7 @@ static ssize_t device_read( struct file* file, char __user* buffer, size_t lengt
     head = NULL;
     curr = NULL;
     result_node = find(current_channel, slots[current_slot_index].channels);
-    printk("inside read() slot number = %d , slot index = %d , channel = %ld" c,urrent_minor, current_slot_index, current_channel);
+    printk("inside read() slot number = %d , slot index = %d , channel = %ld" ,current_minor, current_slot_index, current_channel);
 
     if (result_node == NULL){// no message exists on the channel
         return -EWOULDBLOCK;
